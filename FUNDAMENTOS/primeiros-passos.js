6d47af4421d5e9a4dd0aba1bd10c15dc1d38728c
Aissa <aissamachado6@gmail.com>
2023-03-05 22:37:26 -0300

    exercicio DIA 1- variaveis feito

---
+++ FUNDAMENTOS/primeiros-passos.js
@@ -0,0 +1,8 @@
+// Crie uma variável chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+// Crie uma variável chamada birthCity e atribua a ela a sua cidade natal.
+// Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+// Utilize o console.log() para imprimir as variáveis que você criou.
+let myName = 'Aissa';
+let birthCity = 'Belo horizonte';
+let birthYear = 2001 ;
+console.log('meu nome é ' + myName + ' minha cidade natal fica em ' + birthCity + ' nascida no ano de: ' + birthYear + '.')
