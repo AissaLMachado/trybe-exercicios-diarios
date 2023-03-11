5cf9579d52b3f7c0fd94ce3c4ee9392ece437639
Aissa <aissamachado6@gmail.com>
2023-03-11 18:36:48 -0300

    adicionado cabeçalho

--- FUNDAMENTOS/exercise-seletores.js
+++ FUNDAMENTOS/exercise-seletores.js
@@ -7,7 +7,7 @@ emergencyTasks.style.backgroundColor = 'rgb(251,148,132)';
 
 
 const noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks')[0];
-noEmergencyTasks.style.backgroundColor = 'rgb(251,148,132)';
+noEmergencyTasks.style.backgroundColor = 'rgb(251,227,91)';
 
 const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
 for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
@@ -16,5 +16,8 @@ for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
 
 const noEmergencyTaskHeaders = document.querySelectorAll('.no-emergency-tasks h3');
 for (let index = 0; index < noEmergencyTaskHeaders.length; index += 1){
-      noEmergencyTaskHeaders[index].style.backgroundColor = 'rgb(165, 0, 243)';
+      noEmergencyTaskHeaders[index].style.backgroundColor = 'rgb(23,38,37)';
 }
+
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'rgba(0,57,52,255)';
